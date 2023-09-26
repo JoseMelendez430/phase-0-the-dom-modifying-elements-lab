64efd7c73bb3230d3ba9c4c6cc98d7f07e8df76c
Jose <joseamelendez1301@gmail.com>
2023-09-25 22:01:32 -0400

    modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+document.addEventListener("DOMContentLoaded", function() {
+    const mainElement = document.querySelector("#main");
+    if (mainElement) {
+      mainElement.remove();
+    }
+  
+    const newHeader = document.createElement("h1");
+    newHeader.id = "victory";
+    newHeader.textContent = "Jose is the champion";
+  
+    document.body.appendChild(newHeader);
+  });
